f526d2bc741985652b9bc6819892321b633d0d16
muhammed-sezer <e230531@metu.edu.tr>
2022-02-04 16:20:53 +0300

    changed for onl scr package

--- build/fener_package_v2/share/fener_package_v2/hook/pythonpath_develop.ps1
+++
@@ -1,3 +0,0 @@
-# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
-
-colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/sezer/FENER/ros2_fener_v2/build/fener_package_v2"
--- build/fener_package_v2/share/fener_package_v2/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/sezer/FENER/ros2_fener_v2/build/fener_package_v2"
