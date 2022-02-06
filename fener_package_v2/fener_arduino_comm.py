a80f25317e8eb2ec6fc9b369eaef60fede2b870c
sezer-muhammed <e230531@metu.edu.tr>
2022-02-06 20:10:14 +0300

    arduino comm improvements

--- fener_package_v2/fener_arduino_comm.py
+++ fener_package_v2/fener_arduino_comm.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 import rclpy #import ROS2
 from rclpy.node import Node #import ROS2's Node
-from std_msgs.msg import Float32MultiArray, Int8MultiArray, Int64MultiArray #will be deleted
+from std_msgs.msg import Int64MultiArray #will be deleted
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Imu
 import numpy as np
 import time
 import serial
 from os import system
+import math
 
 
 NAMESPACE = "Arduino"
@@ -38,7 +39,12 @@ class MyNode(Node):
  
         self.encoder_msg = Int64MultiArray()
 
-        self.create_subscription(Float32MultiArray, "/BNO055/all", self.bno055_callback, 1) #TODO Change it according to IMU PUB
+        self.key_speed = 0
+        self.key_angle = 1500
+        self.aim_angle = 0
+        self.bno055_heading = 0
+
+        self.create_subscription(Imu, "/bno055/bno_data", self.bno055_callback, 1) #TODO Change it according to IMU PUB
         self.create_subscription(Twist, "/cmd_vel", self.keyboard_callback, 1) 
 
         self.encoder_publisher = self.create_publisher(Int64MultiArray, PUBLISHERNAME, 1)
@@ -47,7 +53,7 @@ class MyNode(Node):
 
     def timer_callback(self):
         
-        self.keyboard_packet[0:3] = [self.key_speed, min(3000, max(0, self.key_angle + KEY_ANGLE_BNO_MULTIPLIER * (self.aim_angle - self.bno055_heading))), self.bno055_heading%360]
+        self.keyboard_packet[0:3] = [self.key_speed, min(3000, max(0, self.key_angle + (self.aim_angle - self.bno055_heading))), self.bno055_heading%360]
         
         self.old_encoder = self.encoder
         self.encoder = self.send_to_arduino(self.keyboard_commands, self.keyboard_packet)
@@ -57,14 +63,10 @@ class MyNode(Node):
         diff[diff < 0] = 254
 
         self.total_diff = self.total_diff + diff
-
         self.total_encoder = self.encoder + self.total_diff
-
-        self.encoder_publisher.publish(self.encoder_msg)
-
         self.encoder_msg.data = self.total_encoder.tolist()
 
-
+        self.encoder_publisher.publish(self.encoder_msg)
 
         debug_data = self.ard.readline().decode()
 
@@ -92,7 +94,9 @@ class MyNode(Node):
         
 
     def bno055_callback(self, msg):
-        self.bno055_heading = int(msg.data[3])
+        equation_1 = 2 * (msg.orientation.w * msg.orientation.y - msg.orientation.z * msg.orientation.x)
+        equation_2 = 1 - 2 * (msg.orientation.y * msg.orientation.y + msg.orientation.z * msg.orientation.z)
+        self.bno055_heading = math.atan2(equation_1, equation_2) #TODO TEST İF İTS CORRECT
 
 
 def main(args=None):
