38a02f865a7d9d555554702d6bce5753a73a8016
sezer-muhammed <e230531@metu.edu.tr>
2022-02-15 19:40:37 +0300

    lidar update

--- fener_package_v2/fener_lidar_pub.py
+++ fener_package_v2/fener_lidar_pub.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from rplidar import RPLidar
+import rplidar
 import numpy as np
 import time
 
@@ -13,6 +14,8 @@ NAMESPACE = "rplidar"
 NODENAME = "lidar_pub"
 PUBLISHERNAME = "lidar_raw"
 
+
+
 class lidar_publisher(Node):
     def __init__(self):
         super().__init__(NODENAME, namespace = NAMESPACE)
@@ -20,15 +23,14 @@ class lidar_publisher(Node):
         self.lidar = RPLidar('/dev/ttyUSB0')
         self.lidar_publisher = self.create_publisher(LaserScan, PUBLISHERNAME, 2)
 
-        self.declare_parameter("range_min", 0.4)
-        self.declare_parameter("range_max", 4.0)
+        self.declare_parameter("range_min", 0.2)
+        self.declare_parameter("range_max", 4.3)
         
         self.laser_msg = LaserScan()
         self.laser_msg.header.frame_id = "RP_Lidar"
-        self.laser_msg.angle_min = 0.0 
-        self.laser_msg.angle_max = 2 * np.pi
         self.laser_msg.range_min = self.get_parameter("range_min").value
         self.laser_msg.range_max = self.get_parameter("range_max").value
+        self.bias = 0.135
 
         self.get_logger().info("Lidar ready and node is alive")
 
@@ -37,23 +39,22 @@ class lidar_publisher(Node):
         health = self.lidar.get_health()
         self.get_logger().info(f"Lidars health {health}")
 
-
-
     def timer_callback(self):
         prev_measurement = time.time()
         for i, scan in enumerate(self.lidar.iter_scans()):
             scan = np.array(scan)[:, 1:] # 272 scan per turn
-            self.laser_msg.ranges = (scan[:, 1] / 1000).tolist()
-            self.laser_msg.angle_increment = 2 * np.pi / scan.shape[0]
+
+            self.laser_msg.angle_increment = 2 * np.pi / (scan.shape[0])
+            self.laser_msg.angle_min = 0.0 + self.bias
+            self.laser_msg.angle_max = 2 * np.pi + self.bias 
+            self.laser_msg.ranges = (np.flip(scan[:, 1]) / 1000).tolist()
             self.laser_msg.header.stamp = self.get_clock().now().to_msg()
 
             last_measurement = time.time()
             self.laser_msg.scan_time = last_measurement - prev_measurement
             prev_measurement = last_measurement
-
+            
             self.lidar_publisher.publish(self.laser_msg)
-
-
 
 
 def main(args=None):
