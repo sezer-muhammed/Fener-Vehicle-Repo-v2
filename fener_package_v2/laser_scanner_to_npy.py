fa567a4f668fc8af6641cf3364fc4551f4c25c5b
sezer-muhammed <e230531@metu.edu.tr>
2022-02-19 18:34:00 +0300

    added laser saver

---
+++ fener_package_v2/laser_scanner_to_npy.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+from rplidar import RPLidar
+import rplidar
+import numpy as np
+import time
+
+import rclpy
+from sensor_msgs.msg import LaserScan
+from rclpy.node import Node
+
+from os import system
+import pickle
+
+NAMESPACE = "rplidar"
+NODENAME = "lidar_pub"
+PUBLISHERNAME = "lidar_raw"
+
+
+
+class lidar_publisher(Node):
+    def __init__(self):
+        super().__init__(NODENAME, namespace = NAMESPACE)
+
+        self.history = []
+        self.lidar_publisher = self.subscription = self.create_subscription(LaserScan, '/rplidar/lidar_raw', self.lidar_callback, 10)
+    
+    def lidar_callback(self, msg):
+        max_angle = msg.angle_max
+        min_angle = msg.angle_min
+        angle_increment = msg.angle_increment
+        scan_time = msg.scan_time
+        range_min = msg.range_min
+        range_max = msg.range_max
+
+        ranges = msg.ranges
+
+        ThisTurn = {"max_angle":max_angle, "min_angle":min_angle, "angle_increment":angle_increment, "scan_time":scan_time, "range_min":range_min, "range_max":range_max, "ranges":ranges}
+        self.history.append(ThisTurn)
+        
+        if len(self.history) == 30:
+            with open("laser_data.pickle", "wb") as fp:
+                pickle.dump(self.history, fp)
+
+
+def main(args=None):
+    system("sudo chmod 666 /dev/ttyUSB0")
+    rclpy.init(args=args)
+    node = lidar_publisher()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if  __name__ == "__main__":
+    main()
